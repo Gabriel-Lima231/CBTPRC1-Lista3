8fd8fda76931b9c3e5377ce67d75911ca8f49ac6
Gabriel-Lima231 <lima.gabriel3@aluno.ifsp.edu.br>
2026-06-25 23:47:50 -0300

    Add files via upload

---
+++ Lista3_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor = -1;
+            while (valor <= 0)
+            {
+                Console.Write("Digite um valor positivo: ");
+                valor = double.Parse(Console.ReadLine());
+                if (valor <= 0)
+                {
+                    Console.WriteLine("Erro! O valor deve ser positivo.");
+                }
+            }
+            Console.WriteLine("Valor válido cadastrado: " + valor);
+        }
+    }
+}
